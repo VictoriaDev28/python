0fbd16f48cfa460caba27dc4ccc7894709cade22
Ftrail <vicjoh7733@gmail.com>
2025-08-05 08:21:18 -0700

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,16 @@
+Number1 = input("Enter the 1st num:" )
+num1 = int(Number1)
+sign = input("")
+Number2 = input("Enter the 2nd num: ")
+num2 = int(Number2)
+
+if sign == "+":
+    print( num1 + num2)
+
+if sign == "-":
+        print(num1 - num2)
+if sign == "*":
+        print(num1 * num2)
+if sign == "/":
+        print(num1 / num2)
+    
